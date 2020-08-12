eb6d459e3ce031c7c9c914daabbcf2178a7f9960
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-12 15:24:46 +0300

    Tools_lesson_6_task_3

---
+++ lesson_6/task_3/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /.jpg|png$/,
+                /*картинка находится в корне проекта*/
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+        ],
+    }
+};
