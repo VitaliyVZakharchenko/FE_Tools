ef451b4ec83a34f167b58dbcbd8c18a46aca72c2
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-13 16:25:12 +0300

    Tools_lesson_9_task_1

---
+++ lesson_9/task_1/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: 'eslint-config-airbnb-base',
+    rules: {
+        // 'no-console': 2,
+    },
+    env: {
+        browser: true,
+    }
+    // globals: {
+    //     document: true,
+    //     alert: true
+    // }
+};
